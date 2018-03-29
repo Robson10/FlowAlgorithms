a0474a969799ca387871298eb652c0d6a700ce70
Robson10 <mucharobert23@gmail.com>
2018-03-29 03:11:21 +0200

    Add project files.

---
+++ FlowAlgorithms/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using FlowAlgorithms.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlowAlgorithms
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ObservableCollection<MyToolbarItems> myToolbarItems { get; set; } = new ObservableCollection<MyToolbarItems>()
+        {
+            new MyToolbarItems("New File",0,null,"Icons/NewFile.png"),
+            new MyToolbarItems("Open File",1,null,"Icons/OpenFile.png"),
+            new MyToolbarItems("Save File",2,null,"Icons/SaveFile.png"),
+            new MyToolbarItems("Save As",3,null,"Icons/SaveFileAs.png"),
+            new MyToolbarItems("Open From Server",4,null,"Icons/OpenFileFromServer.png"),
+            new MyToolbarItems("Save On Server",5,null,"Icons/SaveFileOnServer.png"),
+            new MyToolbarItems("Login",6,null,"Icons/Login.png"),
+            new MyToolbarItems("Settings",7,null,"Icons/Settings.png"),
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+
+}
---
+++ FlowAlgorithms/Model/MyToolbarItems.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace FlowAlgorithms.Model
+{
+    //public class MyToolbarItems:Button
+    //{
+    //    public MyToolbarItems(string tooltipText, string path)
+    //    {
+    //        ToolTip = tooltipText;
+    //        Background = new ImageBrush() { ImageSource = new BitmapImage(new Uri(path, UriKind.Relative)), Stretch=Stretch.Uniform};
+    //    }        
+    //}
+    public class MyToolbarItems
+
+    {
+
+        public String Name { get; private set; }
+
+        public Int32 ActionId { get; private set; }
+
+        public ICommand Command { get; private set; }
+
+        public BitmapImage Image { get; private set; }
+
+        public MyToolbarItems(String name, Int32 id, ICommand command, string path)
+
+        {
+            
+            Name = name;
+
+            ActionId = id;
+
+            Command = command;
+
+            Image = new BitmapImage(new Uri(path,UriKind.Relative));
+
+        }
+
+    }
+
+}
+
